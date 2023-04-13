c65ff7d9cda87dbfa03d7f2211b83509d918216f
Daniel Bergquist <daniel.bergquist@hey.com>
2023-04-13 15:05:08 -0600

    Add staff pages

--- project2/Sources/App/routes.swift
+++ project2/Sources/App/routes.swift
@@ -7,8 +7,35 @@ func routes(_ app: Application) throws {
         return req.view.render("home", context)
     }
 
-    app.get("staff") { req async -> String in
-        "Meet our great team"
+    app.get("staff", ":name") { req -> EventLoopFuture<View> in
+        let name = req.parameters.get("name") ?? ""
+
+        // create some dummy data to work with
+        let bios = [
+            "kirk" : "My name is James Kirk and I love snakes.",
+            "picard" : "My name is Jean-Luc Picard and I'm made for fish.",
+            "sisko" : "My name is Benjamin Sisko and I'm all about the budgies.",
+            "janeway" : "My name is Kathryn Janeway and I want to hug every hamster",
+            "archer" : "My name is Jonathan Archer and beagles are my thing."
+        ]
+
+        // define the struct we'll pass to the template
+        struct StaffView: Codable {
+            var name: String?
+            var bio: String?
+        }
+
+        let context: StaffView
+
+        // attempt to find a staff member by this name and fill in our struct
+        if let bio = bios[name] {
+            context = StaffView(name: name, bio: bio)
+        } else {
+            context = StaffView()
+        }
+
+        //render the template with whatever we have
+        return req.view.render("staff", context) 
     }
 
     app.get("contact") { req -> EventLoopFuture<View> in
